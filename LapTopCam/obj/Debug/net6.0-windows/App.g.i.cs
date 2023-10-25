6962f134c8e58a108c6afe37710991fad9141728
전영태 <taeya623@gmail.com>
2023-10-25 09:37:05 +0900

    Vsn파일_이미지추가
    vsn파일 이미지경로가 절대경로여서 Path 수정해야 정상작동합니다.

--- LapTopCam/obj/Debug/net6.0-windows/App.g.i.cs
+++ LapTopCam/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace LapTopCam {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/LapTopCam;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LapTopCam;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- LapTopCam/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ LapTopCam/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -92,7 +92,7 @@ namespace LapTopCam {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/LapTopCam;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/LapTopCam;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
